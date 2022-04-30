b103019db9edfc58492aebcb250fcaee7bc46748
jnizama <jorgenizamarios@gmail.com>
2022-04-30 03:03:20 -0500

    swift básico y avanzado 1

--- Tutor01.playground/Contents.swift
+++ Tutor01.playground/Contents.swift
@@ -273,3 +273,122 @@ var aleatorio = arc4random_uniform(100)  //aleatorio numbers
 //-------->var aleatorio = Int.random(in: 0...100)
 print(aleatorio)
  
+func devolver(fullName name:String) -> String {
+    //print("This is a test for \(name) friend")
+    return "This is a test for \(name) friend";
+}
+print(devolver(fullName: "Jorge"))
+
+//Closures, permite encapsular variables y comportamiento que puede ser usado por
+//otras partes del programa sin declarar variables globales.
+//El closure aún preserva el valor y el acceso a la variable.
+//Los closures son una propiedad exclusiva de las funciones.
+//Para crear un closure, primero debes ejecutar una función
+//en un ámbito diferente a ese en el cuál se definió.
+
+var myFunctionWithClosure = {
+    (weight: Double, height: Double, year: Double) -> Double in
+        let w = weight
+        let h = height
+        let y = year
+        let result = (w*h)/y
+    return result
+}
+print("The result is: \(String(myFunctionWithClosure(130, 180, 2000)))")
+
+//Optionals
+//You can accept or not nil (null) to handle errors.
+
+var myOptional: String?
+if let opt = myOptional {
+    print(opt) //En esta instancia, ya se ha desempaquetado (unwrapped) the value
+}
+//encadenamiento de enlaces opcionales.
+var myOptional1: String?
+var myOptional2: String?
+if let opt1 = myOptional1, let  opt2 = myOptional2 {
+    print("the variable has any value: \(opt1) and  \(opt2)")
+}
+else {
+    print("opt1 or opt2 has not value")
+}
+
+//desempaquetado forzado (forcing unwrapped) but it could cause a crash
+var pos_X : Int?
+pos_X = 15
+print("The value is: \(pos_X!) value")
+
+//Podemos crear clases sin inicializador poniendo sus atributos OPTIONALS (?)
+class Student {
+    var name : String?
+    var book : Book?
+}
+
+class Book {
+    var page : Int?
+    var name : String?
+}
+
+let myStudent = Student()
+//Permite acceder avariables opcionales de forma seguro
+if let hasPages = myStudent.book?.name{
+    print(hasPages)
+}
+//podemos encadenar diferentes condiciones
+let myStudent2 = Student()
+//Permite acceder avariables opcionales de forma seguro
+if let hasName = myStudent2.book?.name, let hasBook = myStudent2.book {
+        print("Tiene nombre de estudiante y libro \(hasName) \(hasBook)")
+}
+else if let hasName = myStudent2.book?.name {
+        print("Tiene nombre de estudiante \(hasName)")
+}
+else{
+        print("There is not value neither student or book")
+    }
+
+//Podemos mejorar el manejo de error cuando es nulo usando
+//guard let el cual permite salir de forma segura
+var fullNameAdress: String?
+func myFuncion() {
+    guard let hasName = fullNameAdress else {
+        print("hasName is null")
+        return
+    }
+    print("hay valor en \(hasName) !!!")
+}
+myFuncion()
+fullNameAdress = "Calle la Florida 170"
+myFuncion()
+
+//Errors
+enum SumaErrors: Error {
+    case firstNumberNil
+    case secondNumberNil
+    case numberNegative(num1: Int, num2: Int)
+}
+
+func sumaNumeros(num1: Int?, num2: Int?) throws -> Int {
+    if num1 == nil{
+        throw SumaErrors.firstNumberNil
+    }else if num2 == nil{
+        throw SumaErrors.secondNumberNil
+    }
+    else if num1! < 0 || num2! < 0{
+        throw SumaErrors.numberNegative(num1: num1!, num2: num2!)
+    }
+    return num1! + num2!
+}
+
+//print(try sumaNumeros(num1: 5, num2: nil))
+
+//capturar los errores
+do {
+    print(try sumaNumeros(num1: 5, num2: nil))
+} catch SumaErrors.firstNumberNil {
+    print("El primer numero es nulo")
+} catch SumaErrors.secondNumberNil {
+    print("El segundo numero es nulo")
+} catch SumaErrors.numberNegative(let num1, let num2){
+    print("Algún valor es negativo \(num1) or \(num2) esta mal")
+}
